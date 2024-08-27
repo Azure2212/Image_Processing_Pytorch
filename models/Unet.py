e6df3a9fcee8ce923a619d88f42664b937f38b3b
Azure2212 <dasanbob22122002@gmail.com>
2024-08-27 17:06:56 +0700

    updatE

--- models/Unet.py
+++ models/Unet.py
@@ -1,29 +1,29 @@
 import torch
 import torch.nn as nn
-import torchvision.models as models
 import torch.nn.functional as F
 
+# Define the U-Net model
 class UNET(nn.Module):
     
     def __init__(self, in_channels, classes):
         super(UNET, self).__init__()
+        self.layers = [in_channels, 64, 128, 256, 512, 1024]
         
-        # Load a pretrained ResNet model
-        resnet = models.resnet34(pretrained=True)
-        self.encoder = nn.Sequential(*list(resnet.children())[:-2])  # Remove the final fully connected layers
+        self.double_conv_downs = nn.ModuleList(
+            [self.__double_conv(layer, layer_n) for layer, layer_n in zip(self.layers[:-1], self.layers[1:])])
         
-        # Define decoder layers
-        self.up_trans1 = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
-        self.up_conv1 = self.__double_conv(512, 256)
+        self.up_trans = nn.ModuleList(
+            [nn.ConvTranspose2d(layer, layer_n, kernel_size=2, stride=2)
+             for layer, layer_n in zip(self.layers[::-1][:-2], self.layers[::-1][1:-1])])
+            
+        self.double_conv_ups = nn.ModuleList(
+            [self.__double_conv(layer, layer//2) for layer in self.layers[::-1][:-2]])
         
-        self.up_trans2 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
-        self.up_conv2 = self.__double_conv(256, 128)
-        
-        self.up_trans3 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
-        self.up_conv3 = self.__double_conv(128, 64)
+        self.max_pool_2x2 = nn.MaxPool2d(kernel_size=2, stride=2)
         
         self.final_conv = nn.Conv2d(64, classes, kernel_size=1)
 
+        
     def __double_conv(self, in_channels, out_channels):
         conv = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1, bias=False),
@@ -33,31 +33,28 @@ class UNET(nn.Module):
             nn.ReLU(inplace=True)
         )
         return conv
-
+    
     def forward(self, x):
-        # Encoder
-        x1 = self.encoder[0:4](x)  # Conv1_x
-        x2 = self.encoder[4:6](x1)  # MaxPool2d and Conv2_x
-        x3 = self.encoder[6:8](x2)  # MaxPool2d and Conv3_x
-        x4 = self.encoder[8:10](x3)  # MaxPool2d and Conv4_x
-        x5 = self.encoder[10:12](x4)  # MaxPool2d and Conv5_x
+        # down layers
+        concat_layers = []
         
-        # Decoder
-        x = self.up_trans1(x5)
-        x = F.interpolate(x, size=x4.size()[2:], mode='bilinear', align_corners=False)
-        x = torch.cat([x4, x], dim=1)
-        x = self.up_conv1(x)
+        for down in self.double_conv_downs:
+            x = down(x)
+            if down != self.double_conv_downs[-1]:
+                concat_layers.append(x)
+                x = self.max_pool_2x2(x)
         
-        x = self.up_trans2(x)
-        x = F.interpolate(x, size=x3.size()[2:], mode='bilinear', align_corners=False)
-        x = torch.cat([x3, x], dim=1)
-        x = self.up_conv2(x)
+        concat_layers = concat_layers[::-1]
         
-        x = self.up_trans3(x)
-        x = F.interpolate(x, size=x2.size()[2:], mode='bilinear', align_corners=False)
-        x = torch.cat([x2, x], dim=1)
-        x = self.up_conv3(x)
-        
+        # up layers
+        for up_trans, double_conv_up, concat_layer in zip(self.up_trans, self.double_conv_ups, concat_layers):
+            x = up_trans(x)
+            if x.shape != concat_layer.shape:
+                x = F.interpolate(x, size=concat_layer.shape[2:], mode='bilinear', align_corners=False)
+            
+            concatenated = torch.cat((concat_layer, x), dim=1)
+            x = double_conv_up(concatenated)
+            
         x = self.final_conv(x)
         
         return x
