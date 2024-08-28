ec30206cc6f8b2f9483d0b7a1e1d16b828f82c86
Azure2212 <dasanbob22122002@gmail.com>
2024-08-28 16:11:36 +0700

    updatE

--- trainer/rafdb_multitask_trainer.py
+++ trainer/rafdb_multitask_trainer.py
@@ -388,7 +388,7 @@ class RAFDB_Multitask_Trainer(Trainer):
 
     self.train_loss_list[1].append(cls_train_loss / i)
     self.train_acc_list[1].append(cls_train_acc / i)
-
+    print(f'train_acc_list[1] ={train_acc_list[1]}')
     self.train_total_loss_list.append(train_total_loss / i)
 
 
