c26569f12e802337e728a0d5686d463c52da411f
Azure2212 <dasanbob22122002@gmail.com>
2024-09-13 23:27:11 +0700

    update

--- visualization/rs_rafdb_segmentation.py
+++ visualization/rs_rafdb_segmentation.py
@@ -134,7 +134,6 @@ end = min(args.number_image_test, len(images))
 for idx, (image, gt_mask, pr_mask) in enumerate(zip(images, masks, pr_masks)):
     # Number of samples visualized
     if idx <= args.number_image_test:
-        print(f'vo day {idx}')
         plt.figure(figsize=(20, 6))
         plt.subplot(1, 13, 1)
         image_np = image.cpu().numpy().transpose(1, 2, 0)
