acdcbed4e79d480293bc9f2f05bc4d1c3f03f008
Azure2212 <dasanbob22122002@gmail.com>
2024-09-14 09:05:16 +0700

    update

--- visualization/check_rafdb_data.py
+++ visualization/check_rafdb_data.py
@@ -31,7 +31,7 @@ test_ds = DataLoader(
             )
 stop = 0 
 for i, (images, masks) in tqdm.tqdm(
-        enumerate(test_ds), total=len(test_ds), leave=True, colour="blue", desc=f"Epoch {0}",
+        enumerate(test_ds), total=len(test_ds), leave=True, colour="blue", desc=f"Batch {0}",
         bar_format="{desc}: {percentage:3.0f}%|{bar:50}| {n_fmt}/{total_fmt} [{elapsed}<{remaining}]"
     ):
     batch_size = len(images)  # or len(labels), assuming they have the same length
