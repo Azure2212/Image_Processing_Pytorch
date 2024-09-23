27603c4348bcf4f6a3ef632503bfb46a56a5eeb2
Azure2212 <dasanbob22122002@gmail.com>
2024-09-24 01:09:09 +0700

    update

--- visualization/cm_rafdb_v2.py
+++ visualization/cm_rafdb_v2.py
@@ -66,7 +66,7 @@ elif args.model_name == 'resnet50_vggface2':
     model = resnet50_vggface2()
 elif args.model_name == 'resnet50_vggface2_ft':
     print('resnet50 with pre-train on vggface2(trained on MS1M, and then fine-tuned on VGGFace2) was chose !')
-    model = resnet50_vggface2_ft(use_cbam = True, use_duck = True)
+    model = resnet50_vggface2_ft(pretrained = False, use_cbam = True, use_duck = True)
 elif args.model_name == 'resnet50_imagenet':
     print('resnet50 with pre-train on imagenet was chose !')
     model = resnet50()
