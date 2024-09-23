b6980a8ced0ee0de5297893c5a9fc15ebd414128
Azure2212 <dasanbob22122002@gmail.com>
2024-09-24 02:19:06 +0700

    update

--- models/resnet_v2.py
+++ models/resnet_v2.py
@@ -326,7 +326,7 @@ class CbamBlock(nn.Module):
             channels=channels,
             reduction_ratio=reduction_ratio)
         self.sp_gate = SpatialGate()
-        self.sep = SeparatedConv2DBlock(out_channels, out_channels)
+        self.sep = SeparatedConv2DBlock(channels, channels)
 
     def forward(self, x):
         x = self.ch_gate(x)
