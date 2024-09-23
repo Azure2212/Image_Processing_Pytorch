2336cfa16129f7995d75517f5dae239d2322f86d
Azure2212 <dasanbob22122002@gmail.com>
2024-09-23 11:09:54 +0700

    update

--- main_rafdb.py
+++ main_rafdb.py
@@ -11,6 +11,7 @@ import torch.multiprocessing as mp
 import numpy as np
 import tqdm
 import torch.nn as nn
+import torchvision
 from pytorchcv.model_provider import get_model as ptcv_get_model
 
 seed = 113
