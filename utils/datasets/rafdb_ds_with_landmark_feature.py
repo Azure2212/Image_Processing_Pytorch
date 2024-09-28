64d64a17568fe5e698298c1bab8ec3db285e94f4
Azure2212 <dasanbob22122002@gmail.com>
2024-09-28 13:36:02 +0700

    update

--- utils/datasets/rafdb_ds_with_landmark_feature.py
+++ utils/datasets/rafdb_ds_with_landmark_feature.py
@@ -204,4 +204,5 @@ class image_with_landmark_RafDataSet(Dataset):
         if self.data_type == 'train':
             image, feature_landmark = make_augmentation_image_landmark_custom(image, landmarks)
             #landmarks = landmarks[0]
-        return image.transpose(2, 0, 1), feature_landmark
+        print(path)
+        return image.transpose(2, 0, 1), landmarks
