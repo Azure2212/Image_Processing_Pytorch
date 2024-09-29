f7154837a45758e6db761d96797b36801623adc2
Azure2212 <dasanbob22122002@gmail.com>
2024-09-29 12:35:58 +0700

    update

--- utils/datasets/rafdb_ds_with_landmark_feature.py
+++ utils/datasets/rafdb_ds_with_landmark_feature.py
@@ -73,53 +73,50 @@ class image_with_landmark_RafDataSet(Dataset):
         return detected_faces
 
     def get_landmarks(self, image, detected_faces):
-        landmarks = []
-        landmarks_scores = []
+
         if(detected_faces == None):
             print(f'path None: {path}')
             return None
         
-        def get_landmarks(image, detected_faces):
-            for i, d in enumerate(detected_faces):
-                center = np.array(
-                    [d[2] - (d[2] - d[0]) / 2.0, d[3] - (d[3] - d[1]) / 2.0])
-                center[1] = center[1] - (d[3] - d[1]) * 0.12
-                scale = (d[2] - d[0] + d[3] - d[1]) / self.fa_model.face_detector.reference_scale
-                inp = crop(image, center, scale)
-                inp = torch.from_numpy(inp.transpose(
-                    (2, 0, 1))).float()
-                inp = inp.to(device ='cpu', dtype=torch.float32)
-                inp.div_(255.0).unsqueeze_(0)
-                out = fa.face_alignment_net(inp).detach()
-                # print(out.shape)
-                # out = out.to(device='cpu', dtype=torch.float32).numpy()
-                # print(out.shape)
-                # pts, pts_img, scores = get_preds_fromhm(out, center, scale)
-                # print(pts.shape)
-                # pts, pts_img = torch.from_numpy(pts), torch.from_numpy(pts_img)
-                # print(pts.shape)
-                # pts, pts_img = pts.view(68, 2) * 4, pts_img.view(68, 2)
-                # scores = scores.squeeze(0)
-                # print(pts.shape)
-                # landmarks.append(pts_img.numpy())
-                # landmarks_scores.append(scores)
-                return out
+    def get_landmarks(image, detected_faces):
+        for i, d in enumerate(detected_faces):
+            center = np.array(
+                [d[2] - (d[2] - d[0]) / 2.0, d[3] - (d[3] - d[1]) / 2.0])
+            center[1] = center[1] - (d[3] - d[1]) * 0.12
+            scale = (d[2] - d[0] + d[3] - d[1]) / self.fa_model.face_detector.reference_scale
+            inp = crop(image, center, scale)
+            inp = torch.from_numpy(inp.transpose(
+                (2, 0, 1))).float()
+            inp = inp.to(device ='cpu', dtype=torch.float32)
+            inp.div_(255.0).unsqueeze_(0)
+            out = fa.face_alignment_net(inp).detach()
+            # print(out.shape)
+            # out = out.to(device='cpu', dtype=torch.float32).numpy()
+            # print(out.shape)
+            # pts, pts_img, scores = get_preds_fromhm(out, center, scale)
+            # print(pts.shape)
+            # pts, pts_img = torch.from_numpy(pts), torch.from_numpy(pts_img)
+            # print(pts.shape)
+            # pts, pts_img = pts.view(68, 2) * 4, pts_img.view(68, 2)
+            # scores = scores.squeeze(0)
+            # print(pts.shape)
+            # landmarks.append(pts_img.numpy())
+            # landmarks_scores.append(scores)
+            return out
         
     def __getitem__(self, idx):
         path = self.file_paths[idx]
         image = cv2.imread(path)[:,:,::-1]
         print(image.shape)
         image, detected_faces = make_augmentation_image_landmark_boundingbox_custom(image.copy(), task='resize')
-        if(detected_faces == None):
-            print('ua ko co phat hien khuon mat v')
         landmarks  = self.get_landmarks(image.copy(), detected_faces)
         if(landmarks == None):
-            #print(f'none: {path}')
+            print(f'none: {path}')
             return None, None
         if self.data_type == 'train':
             image = make_augmentation_image_landmark_boundingbox_custom(image.copy())
             #landmarks = landmarks[0]
-        print('vo day3')
+
         image = self.transform(image)
         print(landmarks.shape)
         return image, landmarks
