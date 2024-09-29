5cd6596846cf9c67098cb1b56954f2cc44d8c481
Azure2212 <dasanbob22122002@gmail.com>
2024-09-29 12:39:27 +0700

    update

--- utils/datasets/rafdb_ds_with_landmark_feature.py
+++ utils/datasets/rafdb_ds_with_landmark_feature.py
@@ -89,7 +89,7 @@ class image_with_landmark_RafDataSet(Dataset):
                 (2, 0, 1))).float()
             inp = inp.to(device ='cpu', dtype=torch.float32)
             inp.div_(255.0).unsqueeze_(0)
-            out = fa.face_alignment_net(inp).detach()
+            out = self.fa_model.face_alignment_net(inp).detach()
             # print(out.shape)
             # out = out.to(device='cpu', dtype=torch.float32).numpy()
             # print(out.shape)
