3e6b6ab097f594c37f7d5e8efa97803715fb54ab
Azure2212 <dasanbob22122002@gmail.com>
2024-09-30 22:59:48 +0700

    update

--- trainer/rafdb_trainer.py
+++ trainer/rafdb_trainer.py
@@ -260,6 +260,8 @@ class RAFDB_Trainer(Trainer):
 
     else: #default ="ReduceLROnPlateau"
       self.lr_scheduler_chose = 'None'
+      lambda_lr = lambda epoch: 1.0  # Không thay đổi learning rate
+      self.scheduler = torch.optim.LambdaLR(optimizer, lr_lambda=lambda_lr)
       print("No choosing Learning rate scheduler")
 
 
