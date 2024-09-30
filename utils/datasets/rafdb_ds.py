4e32c36b766acca6b00d61e58dbab339983e8332
Azure2212 <dasanbob22122002@gmail.com>
2024-09-30 15:45:52 +0700

    update

--- utils/datasets/rafdb_ds.py
+++ utils/datasets/rafdb_ds.py
@@ -124,8 +124,8 @@ class RafDataSet(Dataset):
             transforms.ToPILImage(),
             transforms.Resize((224, 224)),
             transforms.ToTensor(),
-            # transforms.Normalize(mean=[0.485, 0.456, 0.406],
-            #                   std=[0.229, 0.224, 0.225]),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                               std=[0.229, 0.224, 0.225]),
 
         ]
         )
