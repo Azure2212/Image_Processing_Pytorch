4f6c61a33d8395cdc16c97af35a9871b1f071c40
Azure2212 <dasanbob22122002@gmail.com>
2024-10-07 00:52:37 +0700

    update

--- models/resnet_v2.py
+++ models/resnet_v2.py
@@ -355,8 +355,8 @@ class CbamBlock(nn.Module):
         super(CbamBlock, self).__init__()
         self.use_duck = use_duck
         if self.use_duck == True:
-            self.wides = WidescopeConv2DBlock_upgrate(channels, channels)
-            #self.mids = MidscopeConv2DBlock_upgrate(channels, channels)
+            #self.wides = WidescopeConv2DBlock_upgrate(channels, channels)
+            self.mids = MidscopeConv2DBlock_upgrate(channels, channels)
             self.sep = SeparatedConv2DBlock_upgrate(channels, channels)
             
 
@@ -368,8 +368,8 @@ class CbamBlock(nn.Module):
     def forward(self, x):
 
         if self.use_duck == True:
-            x = self.wides(x)
-            #x = self.mids(x)
+            #x = self.wides(x)
+            x = self.mids(x)
             x = self.sep(x)
         x = self.ch_gate(x)
         x = self.sp_gate(x)
