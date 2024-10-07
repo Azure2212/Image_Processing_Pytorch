198d40399c188d121a81bb84325da1f6c1e3c92c
Azure2212 <dasanbob22122002@gmail.com>
2024-10-07 23:53:56 +0700

    update

--- main_rafdb_v5.py
+++ main_rafdb_v5.py
@@ -21,7 +21,7 @@ np.random.seed(seed)
 torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
 
-from utils.datasets.rafdb_ds import RafDataSet
+from utils.datasets.rafdb_ds_v5 import RafDataSet
 
 from models.resnet_v5 import resnet50, resnet50_vggface2, resnet50_vggface2_ft
 
