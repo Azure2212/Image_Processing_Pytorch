d231a74785ce89119a1a01754face3718f4c5bd8
Azure2212 <dasanbob22122002@gmail.com>
2025-01-18 00:06:39 +0700

    update

--- models/resnet_v2.py
+++ models/resnet_v2.py
@@ -496,7 +496,7 @@ class ResNet(nn.Module):
             )
 
         layers = []
-        layers.append(block(self.inplanes, planes, stride, downsample, use_cbam = self.use_cbam, use_duck = self.use_duck))
+        layers.append(block(self.inplanes, planes, stride, downsample, use_cbam = self.use_cbam))
         self.inplanes = planes * block.expansion
         for i in range(1, blocks):
             layers.append(block(self.inplanes, planes, use_cbam = self.use_cbam))
