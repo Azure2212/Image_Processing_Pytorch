8f160dc3d770ef20fdb3bdabc4bf610a2966446a
Azure2212 <dasanbob22122002@gmail.com>
2025-01-20 01:42:28 +0700

    update

--- main_rafdb_v2.py
+++ main_rafdb_v2.py
@@ -82,8 +82,8 @@ elif args.model_name == 'resnet50_vggface2':
 elif args.model_name == 'resnet50_vggface2_ft':
     print('resnet50 with pre-train on vggface2(trained on MS1M, and then fine-tuned on VGGFace2) was chose !')
     model = resnet50_vggface2_ft(pretrained = True if args.use_pretrained == 1 else False, use_cbam = True if args.use_cbam == 1 else False, use_duck = True if args.use_duck == 1 else False, load_weight_path = args.load_weight_path)
-    #for name, layer in model.named_children():
-     #   print(f"{name}: {layer}")
+    for name, layer in model.named_children():
+       print(f"{name}: {layer}")
 
     if args.freeze_cbam == 1:
     
